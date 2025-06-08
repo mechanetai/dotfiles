4aa80b6f2935c765d81131b8e2d2ee6d2befb8cc
mechanetai <hheribe2236@gmail.com>
2025-06-09 00:17:53 +0900

    add: zoxide

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -10,68 +10,64 @@ let
   zshManagerPath = "${homeDir}/.config/zsh/.zsh.d/zsh-d-manager.sh";
 in
 {
-  home.username = username;
-  home.homeDirectory = homeDir;
-
-  home.stateVersion = "24.11";
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
+  home = {
+    stateVersion = "25.05";
+    username = username;
+    homeDirectory = homeDir;
+    packages = with pkgs; [
+      git
+      gh
+      fzf
+      tree
+      nerd-fonts.fira-code
+      devbox
+      sqlfluff
+      nixfmt-rfc-style # for VSCode format
+      uv
+      volta
+      zoxide
+    ];
+    # 実態のhome.nixとの相対位置から、特定の位置へのシンボリックリンクを貼る
+    file = {
+      ".config/nvim".source = ../nvim;
+      ".config/python".source = ../python;
+      ".config/zsh/.zsh.d".source = ../zsh/.zsh.d;
+      ".config/starship.toml".source = ../starship.toml;
+    };
+    sessionVariables = {
+      PYTHONSTARTUP = "${homeDir}/.config/python/pythonstartup.py";
+      VOLTA_HOME = "${homeDir}/.volta";
+    };
+    sessionPath = [
+      "$VOLTA_HOME/bin"
+    ];
   };
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+    };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true; # zsh-completionsを有効化
-    autosuggestion.enable = true; # zsh-autosuggestionsを有効化
-    syntaxHighlighting.enable = true; # zsh-syntax-highlightingを有効化
-    dotDir = ".config/zsh"; # zshの設定ファイルを格納するディレクトリを指定
-    initContent = ''
-      export VOLTA_HOME="${homeDir}/.volta"
-      export PYTHONSTARTUP="${homeDir}/.config/python/pythonstartup.py"
-      export PATH="$VOLTA_HOME/bin:$PATH"
-      [ -e ${nixDaemonPath} ] && . ${nixDaemonPath}
-      [ -e ${nixProfilePath} ] && . ${nixProfilePath}
-      [ -e ${zshManagerPath} ] && . ${zshManagerPath}
-    ''; # nix(-daemon).shはNixの環境変数を読み込むためのスクリプト(home-managerを使うため)
+    zsh = {
+      enable = true;
+      enableCompletion = true; # zsh-completionsを有効化
+      autosuggestion.enable = true; # zsh-autosuggestionsを有効化
+      syntaxHighlighting.enable = true; # zsh-syntax-highlightingを有効化
+      dotDir = ".config/zsh"; # zshの設定ファイルを格納するディレクトリを指定
+      initContent = ''
+        export VOLTA_HOME="${homeDir}/.volta"
+        export PYTHONSTARTUP="${homeDir}/.config/python/pythonstartup.py"
+        export PATH="$VOLTA_HOME/bin:$PATH"
+        [ -e ${nixDaemonPath} ] && . ${nixDaemonPath}
+        [ -e ${nixProfilePath} ] && . ${nixProfilePath}
+        [ -e ${zshManagerPath} ] && . ${zshManagerPath}
+      ''; # nix(-daemon).shはNixの環境変数を読み込むためのスクリプト(home-managerを使うため)
+    };
+
+    starship.enable = true;
+    home-manager.enable = true;
   };
-
-  programs.starship.enable = true;
-  programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
-    git
-    gh
-    fzf
-    tree
-    #lunarvim
-    #nerd-fonts._0xproto
-    #nerd-fonts.droid-sans-mono
-    nerd-fonts.fira-code
-    devbox
-    sqlfluff
-    nixfmt-rfc-style # for VSCode format
-    uv
-    volta
-  ];
-
-  # 実態のhome.nixとの相対位置から、特定の位置へのシンボリックリンクを貼る
-  home.file = {
-    ".config/nvim".source = ../nvim;
-    ".config/python".source = ../python;
-    ".config/zsh/.zsh.d".source = ../zsh/.zsh.d;
-    ".config/starship.toml".source = ../starship.toml;
-  };
-
-  home.sessionVariables = {
-    PYTHONSTARTUP = "${homeDir}/.config/python/pythonstartup.py";
-    VOLTA_HOME = "${homeDir}/.volta";
-  };
-
-  home.sessionPath = [
-    "$VOLTA_HOME/bin"
-  ];
 }
